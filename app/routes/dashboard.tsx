8f18befa9644b5bb2c75bf74c2b46bf9cd270038
Eric Almendral <eralmendral@gmail.com>
2022-02-28 14:39:53 +0800

    add sales details to outlet context

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,25 +1,27 @@
 import { Outlet, LoaderFunction, useLoaderData } from 'remix';
 import AppTopbar from '~/Components/Dashboard/AppTopbar';
 import AppMenu from '~/Components/Dashboard/AppMenu';
+
 import { menu } from '~/utils/menus'
 // import { getUser } from '~/services/db.service'
-import { getSalesSummary } from '~/services/firebase.service.js'
+import { getSalesSummary , getSalesDetails} from '~/services/firebase.service.js'
 
 export const loader: LoaderFunction = async () => {
     // const user: any = await getUser();
-    const sales = await getSalesSummary('confluence')
-
+    const salesSummary = await getSalesSummary('confluence')
+    const salesDetails = await getSalesDetails('confluence')
     return {
         menu,
         // user,
-        sales
+        salesSummary,
+        salesDetails
     }
 }
 
 export default function Dashboard() {
     // const { menu, user,  } = useLoaderData();
-    const { menu, sales  } = useLoaderData();
- 
+    const { menu, salesSummary, salesDetails  } = useLoaderData();
+  
     return <div className='layout-wrapper layout-static'>
         <AppTopbar />
         <div className="layout-sidebar" >
@@ -27,7 +29,7 @@ export default function Dashboard() {
         </div>
 
         <div className="layout-main-container">
-            <Outlet context={{ sales }} />
+            <Outlet context={{ salesSummary, salesDetails }} />
         </div>
     </div>
 }
